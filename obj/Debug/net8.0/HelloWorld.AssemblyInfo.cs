49878d563b8def3e75f5ad59a98b82b2047172f3
Lunabi-chan <100802282+Lunabi-chan@users.noreply.github.com>
2024-07-29 13:19:48 +0200

    New

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75cb12ae90551d826d94071295d0f17d1d5e72af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa34ef4c796d12dde348ec33d432f3f67235c9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
